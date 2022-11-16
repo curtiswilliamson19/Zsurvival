c74a8597c7a6d4cae887213e6bd3b9bdc152f9b4
curtiswilliamson19 <curtis.williamson31@gmail.com>
2022-11-16 14:03:49 +0000

    Add project files.

---
+++ Zsurvival/MainGame.cpp
@@ -0,0 +1,49 @@
+#define PLAY_IMPLEMENTATION
+#define PLAY_USING_GAMEOBJECT_MANAGER
+#include "Play.h"
+
+int DISPLAY_WIDTH = 1280;
+int DISPLAY_HEIGHT = 720;
+int DISPLAY_SCALE = 1;
+
+struct GameState
+{
+	float timer = 0;
+	int spriteId = 0;
+};
+
+GameState gameState;
+
+
+enum GameObjectType
+{
+	TYPE_NULL = -1,
+	TYPE_PLAYER,
+};
+
+// The entry point for a PlayBuffer program
+void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
+{
+	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
+}
+
+// Called by PlayBuffer every frame (60 times a second!)
+bool MainGameUpdate( float elapsedTime )
+{
+
+	gameState.timer += elapsedTime;
+	Play::ClearDrawingBuffer( Play::cOrange );
+
+	Play::DrawDebugText( { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, "Hello World!" );
+
+	Play::PresentDrawingBuffer();
+	return Play::KeyDown( VK_ESCAPE );
+}
+
+// Gets called once when the player quits the game 
+int MainGameExit( void )
+{
+	Play::DestroyManager();
+	return PLAY_OK;
+}
+

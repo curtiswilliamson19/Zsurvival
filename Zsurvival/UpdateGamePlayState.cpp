77e7571d2267430f55292058c2fb7eda4bbc3968
curtiswilliamson19 <curtis.williamson31@gmail.com>
2023-07-14 13:30:29 +0100

    Display ammo from player class

--- Zsurvival/UpdateGamePlayState.cpp
+++ Zsurvival/UpdateGamePlayState.cpp
@@ -21,7 +21,7 @@ void UpdateGamePlayState()
 
 	case STATE_PLAY:
 		Play::DrawFontText("64px", "SCORE " + std::to_string((int)gameState.score), { DISPLAY_WIDTH - 70, 20 }, Play::CENTRE);
-		Play::DrawFontText("64px", "AMMO " + std::to_string((int)gameState.ammo), { 55, 20 }, Play::CENTRE);
+		Play::DrawFontText("64px", "AMMO " + std::to_string((int)player.getAmmo()), {55, 20}, Play::CENTRE);
 
 		UpdatePlayer();
 		UpdatePlayerMovement('A', 'D', 'W', 'S');

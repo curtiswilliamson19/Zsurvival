8beeccbe310b10c8bd2a06e07d4ea269c9b71ee3
curtiswilliamson19 <williamson2974@gmail.com>
2023-07-14 11:27:09 +0100

    Added a player class

--- Zsurvival/Player.cpp
+++ Zsurvival/Player.cpp
@@ -1,7 +1,68 @@
 #define PLAY_USING_GAMEOBJECT_MANAGER
 #include "Play.h"
 #include "MainGame.h"
+#include "Player.h"
 
+//constructors
+Player::Player()
+{
+	this->health = 100;
+	this->speed = 3.5f;
+	this->ammo = 10;
+}
+
+Player::Player(int health, float speed, int ammo)
+{
+	this->health = health;
+	this->speed = speed;
+	this->ammo = ammo;
+}
+
+//getters
+int Player::getHealth()
+{
+	return this->health;
+}
+
+float Player::getSpeed()
+{
+	return this->speed;
+}
+
+int Player::getAmmo()
+{
+	return this->ammo;
+}
+
+//setters
+void Player::setHealth(int health)
+{
+	this->health = health;
+}
+
+void Player::setSpeed(float speed)
+{
+	this->speed = speed;
+}
+
+void Player::setAmmo(int ammo)
+{
+	this->ammo = ammo;
+}
+
+//increase ammo by specified amount
+void Player::increaseAmmo(int ammo)
+{
+	this->ammo += ammo;
+}
+
+//decrease ammo by specified amount
+void Player::decreaseAmmo(int ammo)
+{
+	this->ammo -= ammo;
+}
+
+//other
 void UpdatePlayer() 
 {
 	GameObject& obj_player = Play::GetGameObjectByType(TYPE_PLAYER);
--- Zsurvival/Player.h
+++ Zsurvival/Player.h
@@ -1,3 +1,28 @@
 #pragma once
+class Player
+{
+private:
+	int health;
+	float speed;
+	int ammo;
+public:
+	//constructors
+	Player();
+	Player(int health, float speed, int ammo);
+
+	//getters
+	int getHealth();
+	float getSpeed();
+	int getAmmo();
+
+	//setters
+	void setHealth(int health);
+	void setSpeed(float speed);
+	void setAmmo(int ammo);
+
+	//other
+	void increaseAmmo(int ammo);
+	void decreaseAmmo(int ammo);
+};
 
 void UpdatePlayer();

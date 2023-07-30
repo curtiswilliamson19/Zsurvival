6a1230778d9e68507e0c424c6c57f29106c5d791
curtiswilliamson19 <williamson2974@gmail.com>
2023-07-30 10:03:57 +0100

    Fix bug where zombies health not updated correctly

--- Zsurvival/ZombieHealth.cpp
+++ Zsurvival/ZombieHealth.cpp
@@ -5,14 +5,17 @@ void UpdateZombieHealth(int id)
 {
 	for (int i = 0; i < gameState.vZombies.size(); i++)
 	{
-		if (gameState.vZombies[i].getHealth() > 0)
+		if (id == gameState.vZombies[i].getId())
 		{
-			gameState.vZombies[i].decreaseHealth(50);
-		}
+			if (gameState.vZombies[i].getHealth() > 0)
+			{
+				gameState.vZombies[i].decreaseHealth(50);
+			}
 
-		if (gameState.vZombies[i].getHealth() <= 0)
-		{
-			RemoveZombieFromVector(id);
+			if (gameState.vZombies[i].getHealth() <= 0)
+			{
+				RemoveZombieFromVector(id);
+			}
 		}
 	}
 }

e6b5545e2b94408d2a63f65140fd6f656d8eae85
curtiswilliamson19 <williamson2974@gmail.com>
2023-08-06 19:00:42 +0100

    Functionality added to UpdateZombieMovement to allow different speed for zombie types

--- Zsurvival/ZombieMovement.cpp
+++ Zsurvival/ZombieMovement.cpp
@@ -3,6 +3,7 @@
 #include "MainGame.h"
 
 constexpr float ZOMBIE_WALK_SPEED = 2.0f;
+constexpr float ZOMBIE_BIG_WALK_SPEED = 1.0f;
 
 void UpdateZombieMovement() 
 {
@@ -14,7 +15,18 @@ void UpdateZombieMovement()
 	for (int id : vZombies)
 	{
 		GameObject& obj_zombie = Play::GetGameObject(id);
-		Play::PointGameObject(obj_zombie, ZOMBIE_WALK_SPEED, obj_player.pos.x, obj_player.pos.y);
+
+		for (int i = 0; i < gameState.vZombies.size(); i++)
+		{
+			if (id == gameState.vZombies[i].getId() && gameState.vZombies[i].getType() == Zombie::STANDARD)
+			{
+				Play::PointGameObject(obj_zombie, gameState.vZombies[i].getSpeed(), obj_player.pos.x, obj_player.pos.y);
+			}
+			else if (id == gameState.vZombies[i].getId() && gameState.vZombies[i].getType() == Zombie::BIG)
+			{
+				Play::PointGameObject(obj_zombie, ZOMBIE_BIG_WALK_SPEED, obj_player.pos.x, obj_player.pos.y);
+			}	
+		}
 		obj_zombie.pos += obj_zombie.velocity;
 	}
 	
